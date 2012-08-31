cef3955523759c3f6d56777d5ca4fbf1570e0ea6
Francisco de la Peña <frandelapena@gmail.com>
2012-08-31 16:03:53 +0100

    Updated doc release: dev

--- dev/examples/data_navigation/line_spectrum.py
+++ dev/examples/data_navigation/line_spectrum.py
@@ -1,17 +1,27 @@
 """Creates a line spectrum and plots it
 """
 
-# If running from hyperspy's interactive the next two imports can be omitted 
-# omitted (i.e. the next 2 lines)
-import numpy as np
-import matplotlib.pyplot as plt
+# Create a line spectrum with random data
+s = signals.Spectrum({'data' : np.random.random((100,1024))})
 
-from hyperspy.signals.spectrum import Spectrum
+# Define the axis properties
+s.axes_manager.signal_axes[0].name = 'Energy'
+s.axes_manager.signal_axes[0].units = 'eV'
+s.axes_manager.signal_axes[0].scale = 0.3
+s.axes_manager.signal_axes[0].offset = 100
 
-s = Spectrum({'data' : np.random.random((100,1024))})
+s.axes_manager.navigation_axes[0].name = 'time'
+s.axes_manager.navigation_axes[0].units = 'fs'
+s.axes_manager.navigation_axes[0].scale = 0.3
+s.axes_manager.navigation_axes[0].offset = 100
+
+# Give a title
+s.mapped_parameters.title = 'Random line spectrum'
+
+# Plot it
 s.plot()
 
-# If running from hyperspy's interactive console the next line can be 
-# omitted
-plt.show()
+show() # No necessary when running in the HyperSpy's IPython profile
+
+
 
