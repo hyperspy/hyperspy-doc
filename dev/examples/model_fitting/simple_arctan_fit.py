21535c60ca409d9ce01e68da9ad918035a524ad3
Francisco de la Peña <frandelapena@gmail.com>
2014-06-17 10:57:12 +0100

    Updated doc release: dev

---
+++ dev/examples/model_fitting/simple_arctan_fit.py
@@ -0,0 +1,29 @@
+"""Creates a spectrum, and fits an arctan to it."""
+
+import numpy as np
+
+# Generate the data and make the spectrum
+s = signals.SpectrumSimulation(
+    np.arctan(np.arange(-500, 500)))
+s.axes_manager[0].offset = -500
+s.axes_manager[0].units = ""
+s.axes_manager[0].name = "x"
+s.metadata.General.title = "Simple arctan fit"
+
+s.add_gaussian_noise(0.1)
+
+# Make the arctan component for use in the model
+arctan_component = components.Arctan()
+
+# Create the model and add the arctan component
+m = create_model(s)
+m.append(arctan_component)
+
+# Fit the arctan component to the spectrum
+m.fit()
+
+# Print the result of the fit
+m.print_current_values()
+
+# Plot the spectrum and the model fitting
+m.plot()
